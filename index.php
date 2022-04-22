565832ed6e77edc017e1b18cde4672bf34f2c013
SimoneScapin <simonescap87@gmail.com>
2022-04-22 15:21:32 +0200

    impostando il main solo con PHP

--- index.php
+++ index.php
@@ -12,13 +12,29 @@
 </head>
 <body>
     <?php include_once __DIR__ . '/server/data.php'; ?>
-    <header class="container-fluid d-flex p-1">
+    <header class="container-fluid d-flex p-1 mb-3">
         <div class="row">
             <div class="col-4">
             <img src="<?= $logo;?>" alt="logo" class="img-fluid">
             </div>
         </div>
     </header>
+
+    <main class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <?php foreach($music as $data) { ?>
+        <div class="col">
+            <div class="card">
+                <img src="<?= $data['poster'];?>" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+    </main>
     
 </body>
 </html>
